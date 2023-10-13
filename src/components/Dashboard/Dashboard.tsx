db73a89660bb6b16a25fe5090fd90876f26fd9db
sravaniannamraj <sravani.annamraj@gmail.com>
2023-10-13 11:10:41 -0400

    Initial V1 commit

---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from "react";
+import { defaultPingInterval, defaultMaxFileSize } from "../App/Config";
+import { userLogout } from "../../utils/globalUtility";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Board from "../Board/Board";
+import "./Dashboard.css";
+import { useDispatch, useSelector } from "react-redux";
+import * as actions from "./../../store/UserInfo/actions";
+import { getUserInfo } from "components/Constant/ConstantFunction";
+
+const items = [
+  {
+    name: "Board",
+    href: "/board",
+    key: "board",
+    icon: "fa fa-tasks",
+  },
+];
+
+const Dashboard = () => {
+  const dispatch: any = useDispatch();
+  const userInfo = getUserInfo();
+
+  const [addTodos, setAddTodos] = useState<any>([]);
+  const [boardName, setBoardName] = useState<any>("");
+  const [isBoard, setIsBoard] = useState<any>(true);
+  const [sidebarMenu, setSidebarMenu] = useState<any>("");
+
+  const { boardData } = useSelector(
+    (state: any) => state.AddToDoReducers && state.AddToDoReducers
+  );
+
+  const defaultBoardName = () => {
+    const addedname = {
+      boardName: boardName,
+      user: {
+        id: userInfo?.id,
+      },
+    };
+    dispatch(actions.addBoardName(addedname));
+  };
+
+  useEffect(() => {
+    if ((boardData.length = 0)) {
+      defaultBoardName();
+    }
+    dispatch(actions.getBoardName(userInfo?.id));
+  }, []);
+
+  useEffect(() => {
+    if (boardData) {
+      if (
+        sessionStorage.getItem("board_data") === null ||
+        sessionStorage.getItem("board_data") === "" ||
+        sessionStorage.getItem("board_data") === undefined
+      ) {
+        sessionStorage.setItem("board_data", JSON.stringify(boardData?.[0]));
+      }
+      setAddTodos(boardData);
+      dispatch(actions.getTodoData(userInfo?.id));
+    }
+  }, [boardData]);
+
+  const addInputField = () => {
+    const count = addTodos.length + 1;
+    const defaultBoardName = `boardName${count}`;
+    setAddTodos([
+      ...addTodos,
+      {
+        boardName: defaultBoardName,
+        isEditing: false,
+      },
+    ]);
+  };
+
+  const updateInputFields = (index: any, evnt: any) => {
+    const rows = [...addTodos];
+    rows[index].isEditing = false;
+    const addedname = {
+      boardName: boardName,
+      id:rows[index].id,
+      user: {
+        id: userInfo?.id,
+      },
+    };
+    dispatch(actions.addBoardName(addedname));
+    setAddTodos(rows);
+  };
+
+  const editInputFields = (index: any, evnt: any, data: any) => {
+    const rows = [...addTodos];
+    rows[index].isEditing = true;
+    setAddTodos(rows);
+    // if(data?.id){
+    //   console.log("edit",data)
+    // }
+  };
+
+
+  const navigateToScreen = (index: any, evnt: any, data: any) => {
+    evnt.preventDefault();
+    sessionStorage.setItem("board_id", data.id);
+    sessionStorage.setItem("board_data", JSON.stringify(data));
+    dispatch(actions.getTodoDataBoardId(data?.id, userInfo?.id));
+  };
+
+  const handleInputChange = (index: any, evnt: any) => {
+    const { name, value } = evnt.target;
+    const list: any = [...addTodos];
+    list[index][name] = value;
+    setAddTodos(list);
+    setBoardName(value);
+  };
+
+  const handleOpenBoard = () => {
+    setIsBoard(true);
+  };
+
+  return (
+    <>
+      <div className="dashboard-con row flex-nowrap">
+        <Sidebar
+          addTodos={addTodos}
+          addInputField={addInputField}
+          updateInputFields={updateInputFields}
+          editInputFields={editInputFields}
+          handleInputChange={handleInputChange}
+          navigateToScreen={navigateToScreen}
+        />
+
+        <div className="col container-data">
+          <Header
+            items={items}
+            onLinkClick={(href) => {
+              if (href === "/board") {
+                handleOpenBoard();
+              }
+            }}
+            isBoard={isBoard}
+          />
+          {}
+          <div className="content-container">
+            {isBoard && <Board />}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/Dashboard/Header.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./Dashboard.css";
+
+interface IProps {
+  items: any
+  onLinkClick: (href: string) => void;
+  isBoard: boolean;
+}
+const Header: React.FC<IProps> = (props: IProps) => {
+  const { items, onLinkClick,  isBoard} = props;
+  
+  const location = useLocation();
+
+  const handleLinkClick = (href: string) => {
+    onLinkClick(href); // Call the callback function in the parent component with the href value
+  };
+
+  const tabItem = isBoard ? "Board"  : "";
+
+  return (
+    <nav className="navbar navbar-expand-lg navbarstyle">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ms-auto">
+            {items.map((item: any) => (
+              <li
+                className={item.href === location.pathname ? "nav-item nav-button current-page" : "nav-item nav-button"}
+                key={item.name}
+                aria-current={item.href === location.pathname ? "page" : undefined}
+              >
+                <button 
+                  key={item.key}
+                  className={`button-style ${
+                    item.name === tabItem ? "isActive-btn" : ""
+                  }`}
+                  onClick={() => handleLinkClick(item.href)}
+                  >
+                  <i className={item.icon} aria-hidden="true"> </i>
+                  &nbsp; {item.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
+
